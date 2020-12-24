53b339e37869045686e8838eaf86dc9ddf01680d
luoboovo <fuyu16032001@gmail.com>
2020-12-24 16:03:16 +0800

    第七天--爬取京东(demo)格式 整理

--- 爬取京东/spdier.py
+++ 爬取京东/spdier.py
@@ -6,8 +6,8 @@ import requests
 img_url = 'http://img14.360buyimg.com/n7/jfs/t1/152825/38/10944/164600/5fe218ccE42ca9ff7/d8452f0d20a33f15.jpg'
 
 content = requests.get(
-    url = img_url
+    url=img_url
 ).content
 
-with open('jd_img.jpg','wb') as f :
+with open('jd_img.jpg', 'wb') as f:
     f.write(content)
