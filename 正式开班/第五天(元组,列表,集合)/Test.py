da0b93e9564134d24d86cfe4d25474a71da4a37f
luoboovo <fuyu16032001@gmail.com>
2021-01-08 11:30:27 +0800

    冒泡排序

---
+++ 正式开班/第五天(元组,列表,集合)/Test.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+# 冒泡排序
+def bubbleSort(list_name) -> List:
+    for i in range(len(list_name) - 1):
+        flag = False
+        for j in range(len(list_name) - 1 - i):
+            if list_name[j] > list_name[j + 1]:
+                flag = True
+                list_name[j], list_name[j + 1] = list_name[j + 1], list_name[j]
+        if not flag:
+            break
+    return list_name
+
+
+    # print(bubbleSort([1, 2, 3, 4222, 1, 2, 3]))
