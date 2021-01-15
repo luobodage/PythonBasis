06085112c90842cfc5128b6e228607128a8c3973
luoboovo <fuyu16032001@gmail.com>
2021-01-15 12:17:27 +0800

    学习了面向对象

---
+++ 正式开班/第八天(objectOriented)/1.py
@@ -0,0 +1,9 @@
+"""
+author: luoboovo
+contact: fuyu16032001@gmail.com
+datetime:2021/1/13 15:09
+software: PyCharm
+"""
+"""
+codeFunction:
+"""
---
+++ 正式开班/第八天(objectOriented)/objectOrientedExercise.py
@@ -0,0 +1,4 @@
+# author:luoboovo
+# contact: fuyu16001@gmail.com
+# datetime:2021/1/13 9:56
+# software: PyCharm
