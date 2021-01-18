15817135088e3ba1e3e2ce6473b03d2a133bbdc6
luoboovo <fuyu16032001@gmail.com>
2021-01-18 10:03:43 +0800

    第十天以及第十一天的练习代码

---
+++ 正式开班/第十一天/test.py
@@ -0,0 +1,22 @@
+"""
+author: luoboovo
+contact: fuyu16032001@gmail.com
+datetime:2021/1/18 9:49
+software: PyCharm
+        =    =     =
+         =   =   =
+          =  =  =
+        ===========
+        =   萝    =
+        =   卜    =
+        =   神    =
+        =   保    =
+        =   佑    =
+        =   永    =
+        =   无    =
+        =   bug  =
+         =      =
+          =    =
+             =
+
+"""
---
+++ 正式开班/第十天/test.py
@@ -0,0 +1,24 @@
+"""
+author: luoboovo
+contact: fuyu16032001@gmail.com
+datetime:2021/1/15 13:25
+software: PyCharm
+        =    =     =
+         =   =   =
+          =  =  =
+        ===========
+        =   萝    =
+        =   卜    =
+        =   神    =
+        =   保    =
+        =   佑    =
+        =   永    =
+        =   无    =
+        =   bug  =
+         =      =
+          =    =
+             =
+
+"""
+import datetime
+print(type(datetime.date.today().year))
