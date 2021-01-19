8319063c41825970e7a42c7ce6a705ffc91791ef
luoboovo <fuyu16032001@gmail.com>
2021-01-19 08:38:15 +0800

    介绍一个数据接口

--- 正式开班/第十一天/test.py
+++ 正式开班/第十一天/test.py
@@ -1,7 +1,7 @@
 """
 author: luoboovo
 contact: fuyu16032001@gmail.com
-datetime:2021/1/18 9:49
+datetime: 2021/1/18 9:49
 software: PyCharm
         =    =     =
          =   =   =
@@ -20,3 +20,29 @@ software: PyCharm
              =
 
 """
+import baostock as bs
+import pandas as pd
+
+# 登陆系统
+lg = bs.login()
+# 显示登陆返回信息
+print('login respond error_code:' + lg.error_code)
+print('login respond  error_msg:' + lg.error_msg)
+
+# 获取中证500成分股
+rs = bs.query_zz500_stocks()
+print('query_zz500 error_code:' + rs.error_code)
+print('query_zz500  error_msg:' + rs.error_msg)
+
+# 打印结果集
+zz500_stocks = []
+while (rs.error_code == '0') & rs.next():
+    # 获取一条记录，将记录合并在一起
+    zz500_stocks.append(rs.get_row_data())
+result = pd.DataFrame(zz500_stocks, columns=rs.fields)
+# 结果集输出到csv文件
+result.to_csv("zz500_stocks.csv", encoding="gbk", index=False)
+print(result)
+
+# 登出系统
+bs.logout()
