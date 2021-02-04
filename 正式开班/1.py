c11c59ce6682cfe56b5bbdd7e7d059cdb9be26ea
luoboovo <fuyu16032001@gmail.com>
2021-02-04 10:46:16 +0800

    ajax以及echearts

---
+++ 正式开班/1.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+
+import requests
+# author: luoboovo
+# contact: fuyu16032001@gmail.com
+# datetime: 2021/2/4 8:48
+# software: PyCharm
+#         =    =     =
+#          =   =   =
+#           =  =  =
+#         ===========
+#         =   萝    =
+#         =   卜    =
+#         =   神    =
+#         =   保    =
+#         =   佑    =
+#         =   永    =
+#         =   无    =
+#         =   bug  =
+#          =      =
+#           =    =
+#              =
+
+
+a = requests.get(url='http://jiekou.laiyue.work').json()
+print(a)
